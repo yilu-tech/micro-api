a769ccc6ddb35735e273d75b8b4900319354c129
wpw <developer@yilu.co>
2018-01-30 16:05:46 +0800

    GET method params

--- src/MicroApi.php
+++ src/MicroApi.php
@@ -10,51 +10,44 @@ use GuzzleHttp\Exception\RequestException as GuzzleRequestException;
 use GuzzleHttp\Promise;
 use Yilu\MicroApi\MicroApiRequestException;
 use Yilu\MicroApi\MicroLog;
-
-class MicroApi
-{
+class MicroApi{
     protected $baseUrl = '';
     private $client;
     private $headers;
     private $options;
     private $log;
 
-    public function __construct()
-    {
+    public function __construct(){
         $this->log = new MicroLog();
         $this->headers = ['Content-Type' => 'application/json'];
         $this->options['headers'] = $this->headers;
-        $this->client = new \GuzzleHttp\Client($this->options);
+        $this->client =  new \GuzzleHttp\Client($this->options);
     }
-
-    public function log()
-    {
+    public function log(){
         return $this->log;
     }
 
-    private function makeUrl($uri)
-    {
-        $pathArray = explode('/', $uri);
+    private function makeUrl($uri){
+        $pathArray  = explode('/',$uri);
         $module = $pathArray[0];
         $moduleName = "micro.services.$module";
-        if (\Config::has($moduleName)) { //模块名称存在，从配置文件读取地址
-            $url = config("micro.services.$module") . "/$uri";
-        } else {//否则填写的内容直接作为地址
+        if(\Config::has($moduleName)){ //模块名称存在，从配置文件读取地址
+            $url = config("micro.services.$module")."/$uri";
+        }else{//否则填写的内容直接作为地址
             $url = $uri;
         }
 
         return $url;
     }
 
-    private function runRequest($method, $uri, $data)
-    {
+    private function runRequest($method,$uri,$data){
         $url = $this->makeUrl($uri);
-
+        $request = new Request($method, $url);
         $options = [];
 
-        if (isset($data['form_params'])) {
-            $options = array_merge($options, $data);
-        } else {
+        if(isset($data['form_params'])){
+            $options = array_merge($options,$data);
+        }else{
             $options['json'] = $data;
         }
 
@@ -62,80 +55,73 @@ class MicroApi
         $this->log()->debug('----------------新请求-------------------');
         $this->log()->debug('----------------------------------------');
         $this->log()->debug("Method:$method,  请求地址 $url, 数据 ", $data);
-        try {
-            $response = $this->client->request($method, $url, $options);
-        } catch (GuzzleRequestException $e) {
-            throw new MicroApiRequestException($e,$this);
+        try{
+            $response = $this->client->request($method, $url,$options);
+        }catch (GuzzleRequestException $e){
+            throw new MicroApiRequestException($e);
         }
         return $response;
     }
 
     /**************sync***************/
 
-    function get(String $uri, Array $params = [])
-    {
-        $res = $this->runRequest('GET', $uri, $params);
+    function get(String $uri,Array $params=[],$options=[]){
+        $uri = $uri ."?". http_build_query( $params );
+        $res = $this->runRequest('GET',$uri,$options);
         return new MicroApiResponse($res);
     }
 
-    function post(String $uri, Array $data = [], $options = [])
-    {
-        $res = $this->runRequest('POST', $uri, $data);
+    function post(String $uri,Array $data=[],$options=[]){
+        $res = $this->runRequest('POST',$uri,$data);
+        return new MicroApiResponse($res);
+    }
+    function put(String $uri,Array $data=[]){
+        $res = $this->runRequest('PUT',$uri,$data);
+        return new MicroApiResponse($res);
+    }
+    function patch(String $uri,Array $data=[]){
+        $res = $this->runRequest('PATCH',$uri,$data);
+        return new MicroApiResponse($res);
+    }
+    function delete(String $uri,Array $params=[]){
+        $res = $this->runRequest('DELETE',$uri,$params);
         return new MicroApiResponse($res);
     }
 
-    function put(String $uri, Array $data = [])
-    {
-        $res = $this->runRequest('PUT', $uri, $data);
-        return new MicroApiResponse($res);
-    }
+       /*************async*************/
 
-    function patch(String $uri, Array $data = [])
-    {
-        $res = $this->runRequest('PATCH', $uri, $data);
-        return new MicroApiResponse($res);
-    }
-
-    function delete(String $uri, Array $params = [])
-    {
-        $res = $this->runRequest('DELETE', $uri, $params);
-        return new MicroApiResponse($res);
-    }
-
-    /*************async*************/
-
-    function getAsync(String $uri, Array $params)
-    {
-        $promise = $this->client->getAsync($uri);
-        return $promise;
-    }
-
-    function postAsync(String $uri, Array $params)
-    {
-        $promise = $this->client->getAsync($uri);
-        return $promise;
-    }
-
-    function putAsync(String $uri, Array $params)
-    {
-        $promise = $this->client->getAsync($uri);
-        return $promise;
-    }
-
-    function deleteAsync(String $uri, Array $params)
-    {
-        $promise = $this->client->getAsync($uri);
-        return $promise;
-    }
-
-    //异步执行的暂时不用
-    function promiseRun(Array $promises): Array
-    {
-        $res = Promise\unwrap($promises);
-
-        foreach ($res as $key => $item) {
-            $ret[$key] = new MicroApiResponse($item);
-        }
-        return $ret;
-    }
+       function getAsync(String $uri, Array $params)
+       {
+           $promise = $this->client->getAsync($uri);
+           return $promise;
+       }
+   
+       function postAsync(String $uri, Array $params)
+       {
+           $promise = $this->client->getAsync($uri);
+           return $promise;
+       }
+   
+       function putAsync(String $uri, Array $params)
+       {
+           $promise = $this->client->getAsync($uri);
+           return $promise;
+       }
+   
+       function deleteAsync(String $uri, Array $params)
+       {
+           $promise = $this->client->getAsync($uri);
+           return $promise;
+       }
+   
+       //异步执行的暂时不用
+       function promiseRun(Array $promises): Array
+       {
+           $res = Promise\unwrap($promises);
+   
+           foreach ($res as $key => $item) {
+               $ret[$key] = new MicroApiResponse($item);
+           }
+           return $ret;
+       }
 }
--- src/MicroApiResponse.php
+++ src/MicroApiResponse.php
@@ -4,6 +4,8 @@ namespace Yilu\MicroApi;
 
 
 use GuzzleHttp\Psr7\Response;
+use GuzzleHttp;
+
 class MicroApiResponse{
     protected $res;
 
@@ -12,13 +14,22 @@ class MicroApiResponse{
         $this->res = $res;
     }
 
-    function data(){
-        $body =  \GuzzleHttp\json_decode($this->res->getBody(),true);
-        if(isset($body['data'])){ //解决原PHP数据格式
-            return $body["data"];
-        }else{ //解析java的数据返回
-            return $body;
+    function data()
+    {
+        $data = null;
+        $jsonString = $this->res->getBody()->__toString();
+
+        if ($jsonString && !empty($jsonString)) {
+
+            $body = \GuzzleHttp\json_decode($jsonString, true);
+            if (isset($body['data'])) { //解决原PHP数据格式
+                $data = $body["data"];
+            } else { //解析java的数据返回
+                $data = $body;
+            }
         }
+
+        return $data;
     }
 
     function get(String $key){
